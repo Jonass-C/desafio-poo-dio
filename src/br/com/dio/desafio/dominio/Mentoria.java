dd613800dfb36ea3c2c9e1df0c48ad01c1ad3c55
Jonas <brjonas13@gmail.com>
2025-08-30 13:42:42 -0300

    criando classes

---
+++ src/br/com/dio/desafio/dominio/Mentoria.java
@@ -0,0 +1,26 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Mentoria extends Conteudo{
+
+    LocalDate data;
+
+    public LocalDate getData() {
+        return data;
+    }
+
+    public void setData(LocalDate data) {
+        this.data = data;
+    }
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("Mentoria: %s - Descrição: %s - Data: %s\n", getTitulo(), getDescricao(), data);
+    }
+}
